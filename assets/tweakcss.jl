584cf045316384f1779006e1bcd1c92f24423d46
Tamas K. Papp <tkpapp@gmail.com>
2018-10-20 11:50:07 +0200

    add formatting, row ids

--- assets/tweakcss.jl
+++ assets/tweakcss.jl
@@ -1,8 +1,15 @@
 # generate and open an example table for tweaking CSS
 
-using BrowseTables, Tables
+using BrowseTables
 
-table = collect((a = i, b = Float64(i), c = 'a'-1+i) for i in 1:30)
+table = collect(NamedTuple{(:a,:b,:c), Tuple{Union{Int,Missing},Float64,Union{String,Nothing}}},
+                iszero(i % 10) ? (a = missing, b = NaN, c = nothing) :
+                (a = i, b = Float64(i), c = ('a'+(i%26))^((i%7+1))) for i in 1:100)
 
 open_html_table(table; options = TableOptions(; css_inline = false))
-write_html_table("./example.html", table[1:10])
+
+
+# example table in README
+
+exampletable = collect((a = i, b = Float64(i), c = 'a'-1+i) for i in 1:10)
+write_html_table("./example.html", exampletable)
--- src/BrowseTables.jl
+++ src/BrowseTables.jl
@@ -7,59 +7,8 @@ import DefaultApplication
 using DocStringExtensions: SIGNATURES
 import Tables
 
-function writetags(f, io::IO, tag::AbstractString; bropen = true, brclose = bropen)
-    write(io, "<", tag, ">")
-    bropen || println(io)
-    f(io)
-    write(io, "</", tag, ">")
-    brclose || println(io)
-    nothing
-end
-
-function writeescaped(io, str::AbstractString)
-    for char in str
-        if char == '&'
-            write(io, "&amp")
-        elseif char == '<'
-            write(io, "&lt")
-        elseif char == '>'
-            write(io, "&gt")
-        else
-            write(io, char)
-        end
-    end
-    nothing
-end
-
-writecell(io, x, kind = "td") = writetags(io -> writeescaped(io, string(x)), io, kind)
-
-function writerow(io, nt::NamedTuple)
-    writetags(io, "tr") do io
-        for x in values(nt)
-            writecell(io, x)
-        end
-    end
-end
-
-writecaption(io, ::Nothing) = nothing
-
-writecaption(io, str::AbstractString) = writetags(io -> writeescaped(io, str), io, "caption")
-
-writeschema(io, ::Nothing) = nothing
-
-function writeschema(io, sch::Tables.Schema)
-    writetags(io, "thead") do io
-        writetags(io, "tr") do io
-            foreach(x -> writecell(io, x, "th"), sch.names)
-        end
-    end
-end
-
-function writebody(io, itr)
-    writetags(io, "tbody") do
-        foreach(row -> writerow(io, row), itr)
-    end
-end
+
+# options and customizations
 
 const HTMLHEADSTART = """
 <!DOCTYPE html><html lang="en"><head>
@@ -74,6 +23,98 @@ Base.@kwdef struct TableOptions
     css_inline::Bool = true
 end
 
+
+# low-level HTML construction
+
+function writeescaped(io, str::AbstractString)
+    for char in str
+        if char == '&'
+            write(io, "&amp;")
+        elseif char == '<'
+            write(io, "&lt;")
+        elseif char == '>'
+            write(io, "&gt;")
+        elseif char == '"'
+            write(io, "&quot;")
+        else
+            write(io, char)
+        end
+    end
+    nothing
+end
+
+escapestring(str::AbstractString) = (io = IOBuffer(); writeescaped(io, str); String(take!(io)))
+
+function writetags(f, io::IO, tag::AbstractString; bropen = false, brclose = bropen,
+                   attributes = NamedTuple())
+    write(io, "<", tag)
+    for (k, v) in pairs(attributes)
+        write(io, " ", string(k), "=\"")
+        writeescaped(io, v)
+        write(io, "\"")
+    end
+    write(io, ">")
+    bropen && println(io)
+    f(io)
+    write(io, "</", tag, ">")
+    brclose && println(io)
+    nothing
+end
+
+writecell(io, content::AbstractString; kind = "td", attributes = NamedTuple()) =
+    writetags(io -> writeescaped(io, content), io, kind; attributes = attributes)
+
+
+# cell formatting
+
+"""
+$(SIGNATURES)
+
+HTML representation of a cell, with attributes. Content should be escaped properly, as it is
+written to HTML directly.
+"""
+cellwithattributes(::TableOptions, x) = escapestring(string(x)), NamedTuple()
+
+cellwithattributes(::TableOptions, str::AbstractString) =
+    escapestring(str), (class = "alignleft", )
+
+cellwithattributes(::TableOptions, x::Union{Missing,Nothing}) =
+    escapestring(repr(x)), (class = "aligncenter", )
+
+struct RowId
+    id::Int
+end
+
+cellwithattributes(::TableOptions, rowid::RowId) =
+    escapestring(string(rowid.id)), (class = "rowid", )
+
+
+# table structure
+
+function writerow(io, options::TableOptions, nt::NamedTuple)
+    writetags(io, "tr"; brclose = true) do io
+        for x in values(nt)
+            htmlcontent, attributes = cellwithattributes(options, x)
+            writecell(io, htmlcontent; attributes = attributes)
+        end
+    end
+end
+
+writecaption(io, ::Nothing) = nothing
+
+writecaption(io, str::AbstractString) = writetags(io -> writeescaped(io, str), io, "caption")
+
+writeschema(io, ::Nothing) = nothing
+
+function writeschema(io, sch::Tables.Schema)
+    writetags(io, "thead"; brclose = true) do io
+        writetags(io, "tr") do io
+            writecell(io, "#"; kind = "th") # row id
+            foreach(x -> writecell(io, escapestring(string(x)); kind = "th"), sch.names)
+        end
+    end
+end
+
 function writestyle(io::IO, path::AbstractString, inline::Bool)
     if inline
         writetags(io -> write(io, read(path, String)), io, "style")
@@ -83,20 +124,28 @@ function writestyle(io::IO, path::AbstractString, inline::Bool)
     nothing
 end
 
+
+# high-level API
+
 function write_html_table(filename::AbstractString, table;
-                        title = "Table", caption = nothing, options = TableOptions())
+                          title = "Table", caption = nothing,
+                          options::TableOptions = TableOptions())
     @argcheck Tables.istable(table) "The table should support the interface of Tables.jl."
     open(filename, "w") do io
         write(io, HTMLHEADSTART)
         writetags(io -> writeescaped(io, title), io, "title")
         writestyle(io, options.css_path, options.css_inline)
         println(io, "</head>")    # close manually, opened in HTMLHEADSTART
-        writetags(io, "body") do io
-            writetags(io, "table") do io
+        writetags(io, "body"; bropen = true) do io
+            writetags(io, "table"; bropen = true) do io
                 writecaption(io, caption)
                 rows = Tables.rows(table)
                 writeschema(io, Tables.schema(rows))
-                foreach(row -> writerow(io, row), rows)
+                writetags(io, "tbody"; bropen = true) do io
+                    for (id, row) in enumerate(rows)
+                        writerow(io, options, merge((rowid = RowId(id),), row))
+                    end
+                end
             end
         end
         println(io, "</html>")
